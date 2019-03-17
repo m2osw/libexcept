70ba02f950a5a20a14f6ca493e3a20175f8cf58d
AlexisWilke <alexis@m2osw.com>
2019-03-17 00:06:34 -0700

    Copyright notice updated to 2019.

--- src/exception.cpp
+++ src/exception.cpp
@@ -8,7 +8,7 @@
  *      code quickly.
  *
  * License:
- *      Copyright (c) 2011-2018  Made to Order Software Corp.  All Rights Reserved
+ *      Copyright (c) 2011-2019  Made to Order Software Corp.  All Rights Reserved
  * 
  *      https://snapwebsites.org/
  *      contact@m2osw.com
