3b262422fb6d52c23b564d4e6d3483c47b6c9e76
AlexisWilke <alexis@m2osw.com>
2021-10-19 20:38:10 -0700

    Replaced Catch2 with SnapCatch2.
    Properly use the flag (SNAPCATCH2_FOUND) in the if() statement.
    Removed the link_directories() from the main CMakeLists.txt().
    Removed reference to AUTOMOC in main CMakeLists.txt files (no need in libexcept).
    Fixed the names in header of cmake file; DIRS/LIBS are plural.
    Cleaned up formatting of cmake file.
    Cleaned up some copyright notices.

--- cmake/LibExceptConfig.cmake
+++ cmake/LibExceptConfig.cmake
@@ -3,23 +3,46 @@
 # Once done this will define
 #
 # LIBEXCEPT_FOUND        - System has LibExcept
-# LIBEXCEPT_INCLUDE_DIR  - The LibExcept include directories
-# LIBEXCEPT_LIBRARY      - The libraries needed to use LibExcept (none)
+# LIBEXCEPT_INCLUDE_DIRS - The LibExcept include directories
+# LIBEXCEPT_LIBRARIES    - The libraries needed to use LibExcept (none)
 # LIBEXCEPT_DEFINITIONS  - Compiler switches required for using LibExcept (none)
 
-find_path( LIBEXCEPT_INCLUDE_DIR libexcept/exception.h
-		   PATHS $ENV{LIBEXCEPT_INCLUDE_DIR}
-		   PATH_SUFFIXES libexcept
-		 )
-find_library( LIBEXCEPT_LIBRARY except
-		   PATHS $ENV{LIBEXCEPT_LIBRARY}
-		 )
-mark_as_advanced( LIBEXCEPT_INCLUDE_DIR LIBEXCEPT_LIBRARY )
+find_path(
+    LIBEXCEPT_INCLUDE_DIR
+        libexcept/exception.h
 
-set( LIBEXCEPT_INCLUDE_DIRS ${LIBEXCEPT_INCLUDE_DIR} )
-set( LIBEXCEPT_LIBRARIES    ${LIBEXCEPT_LIBRARY}     )
+    PATHS
+        $ENV{LIBEXCEPT_INCLUDE_DIR}
 
-include( FindPackageHandleStandardArgs )
+    PATH_SUFFIXES
+        libexcept
+)
+
+find_library(
+    LIBEXCEPT_LIBRARY
+        except
+
+    PATHS
+        $ENV{LIBEXCEPT_LIBRARY}
+)
+
+mark_as_advanced(
+    LIBEXCEPT_INCLUDE_DIR
+    LIBEXCEPT_LIBRARY
+)
+
+set(LIBEXCEPT_INCLUDE_DIRS ${LIBEXCEPT_INCLUDE_DIR})
+set(LIBEXCEPT_LIBRARIES    ${LIBEXCEPT_LIBRARY})
+
+include(FindPackageHandleStandardArgs)
+
 # handle the QUIETLY and REQUIRED arguments and set LIBEXCEPT_FOUND to TRUE
 # if all listed variables are TRUE
-find_package_handle_standard_args( LibExcept DEFAULT_MSG LIBEXCEPT_INCLUDE_DIR LIBEXCEPT_LIBRARY )
+find_package_handle_standard_args(
+    LibExcept
+    DEFAULT_MSG
+    LIBEXCEPT_INCLUDE_DIR
+    LIBEXCEPT_LIBRARY
+)
+
+# vim: ts=4 sw=4 et
